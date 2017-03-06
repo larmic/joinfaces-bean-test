d58f0a9aa607f8b1922ceb21d06c94ef01bd81fb
larmic <mail@larmic.de>
2017-03-06 11:26:25 +0100

    fixed some oss.sonartype.org requirements

--- src/main/java/de/larmic/joinfaces/test/JsfSpringBeanBuilder.java
+++ src/main/java/de/larmic/joinfaces/test/JsfSpringBeanBuilder.java
@@ -5,25 +5,28 @@ import org.springframework.context.ApplicationContext;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Simple chaining builder to create JSF scoped bean that should be unit tested.
+ */
 public class JsfSpringBeanBuilder {
 
-   private final Map<String, String> parameterMap = new HashMap<>();
-   private final ApplicationContext context;
+    private final Map<String, String> parameterMap = new HashMap<>();
+    private final ApplicationContext context;
 
-   public JsfSpringBeanBuilder(ApplicationContext context) {
-      this.context = context;
-   }
+    public JsfSpringBeanBuilder(ApplicationContext context) {
+        this.context = context;
+    }
 
-   public JsfSpringBeanBuilder withExternalParameter(String key, String value) {
-      this.parameterMap.put(key, value);
-      return this;
-   }
+    public JsfSpringBeanBuilder withExternalParameter(String key, String value) {
+        this.parameterMap.put(key, value);
+        return this;
+    }
 
-   public <T> T build(Class<T> beanClass) {
-      new FacesContextMock()
-            .withExternalParameter(parameterMap)
-            .replaceIn(context);
+    public <T> T build(Class<T> beanClass) {
+        new FacesContextMock()
+                .withExternalParameter(parameterMap)
+                .replaceIn(context);
 
-      return context.getBean(beanClass);
-   }
+        return context.getBean(beanClass);
+    }
 }
